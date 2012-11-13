28b1ee24c9e5718a0914895307f794825956ffb5
Andy Hausmann <hi@andy-hausmann.de>
2012-11-13 11:16:18 +0100

    Fixed a bug regarding backward compatibility to TYPO3 4.5.x. Thanks to Benjamin K. for calling my attention regarding this issue.

--- Resources/Private/Php/class.jqct_wizicon.php
+++ Resources/Private/Php/class.jqct_wizicon.php
@@ -46,13 +46,21 @@ class jqct_pi1_wizicon
 	/**
 	 * Reads the Backend Localization file.
 	 *
-	 * @return	The array with language labels
+	 * @return array The array with language labels
 	 */
 	protected function includeLocalLang()
 	{
-		$l10nParser = t3lib_div::makeInstance('t3lib_l10n_parser_Llxml');
 		$llFile = t3lib_extMgm::extPath($this->extKey) . 'Resources/Private/Language/locallang_be.xml';
-		return  $l10nParser->getParsedData($llFile, $GLOBALS['LANG']->lang);
+
+		$l10n = t3lib_div::makeInstance('language');
+		$l10n->init($GLOBALS['LANG']->lang);
+		$l10nArr = $l10n->includeLLFile($llFile, false);
+
+		// Removed following lines, because this will only work with TYPO3 4.6+
+		//$l10nParser = t3lib_div::makeInstance('t3lib_l10n_parser_Llxml');
+		//return $l10nParser->getParsedData($llFile, $GLOBALS['LANG']->lang);
+
+		return $l10nArr;
 	}
 
 }
